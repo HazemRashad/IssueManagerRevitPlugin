bc2518319be8668f2ef243b2184f13bab1acb241
Moenes98 <amunes.f98@gmail.com>
2025-05-27 19:16:45 +0300

    Add Generic API Service

--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -9,5 +9,33 @@
             _client = client;
         }
 
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            return await _client.GetFromJsonAsync<T>(url);
+        }
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T data)
+        {
+            return await _client.PostAsJsonAsync(url, data);
+        }
+
+        protected async Task<TResponse?> PostAsync<TRequest, TResponse>(string url, TRequest data)
+        {
+            var response = await _client.PostAsJsonAsync(url, data);
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadFromJsonAsync<TResponse>();
+            return default;
+        }
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T data)
+        {
+            return await _client.PutAsJsonAsync(url, data);
+        }
+
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            return await _client.DeleteAsync(url);
+        }
+
     }
 }
