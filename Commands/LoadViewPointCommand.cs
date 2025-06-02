1e20ec02e8ee79fe214a726cd31d3c3abcb33c05
mostafaelwahsh <mostafanabil447@gmail.com>
2025-06-02 14:49:00 +0300

    #LoadViewPointCommand

--- Commands/LoadViewPointCommand.cs
+++ Commands/LoadViewPointCommand.cs
@@ -6,10 +6,15 @@
     {
         public override void Execute()
         {
+            var httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:44374/") };
+            var issueService = new IssueApiService(httpClient);
+            var lookupService = new LookupApiService(httpClient);
 
-            var viewModel = new LoadViewPointViewModel();
-            var view = new LoadViewPointView(viewModel);
-            WindowRevitHandler.ShowWindow(view);
+            var viewModel = new LoadViewPointViewModel(lookupService, issueService);
+            var view = new LoadViewPointView(viewModel); 
+            WindowRevitHandler.ShowWindow(view); 
         }
+
+
     }
 }
